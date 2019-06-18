489d48347d92e34aeadd041393dfdc7a20f0ed4e
rcbensley <richardbensley@gmail.com>
2019-06-18 14:46:31 +0100

    Working on walk-through demos.

--- tests/demo.py
+++ tests/demo.py
@@ -1,9 +1,37 @@
 from kvdb import Kvdb
-from time import sleep
+from datetime import datetime as dt
+from datetime import timedelta as td
 
 db = Kvdb()
 db.setup()
 
-db.set('a', {'iz_cool': False})
-sleep(2)
-db.set('a', {'iz_cool': True})
+
+weekly_watch_list = [
+    {"title": "Blade Runner", "Director": "Ridley Scott"},
+    {"title": "Frozen", "Director": ["Chris Buck", "Jennifer Lee"]},
+    {"title": "Jumanji", "Director": "Joe Johnston"},
+    {"title": "Labyrinth", "Director": "Jim Henson"},
+    {"title": "Pixels", "Director": "Chris Columbus"},
+    {"title": "Jack and Jill", "Director": "Dennis Dugan"},
+    {"title": "Transformers: The Last knight", "Director": "Michael Bay"}
+]
+
+week_start = dt(year=2019, month=6, day=10)
+week = [week_start + td(days=i) for i in range(0, len(weekly_watch_list))]
+
+
+def gotta_watch_em_all(films):
+    for i in range(0, len(films)):
+        date = week_start + td(days=i)
+        date_key = dt.strftime(date, '%Y-%m-%d')
+        film = weekly_watch_list[i]
+        db.set(k=date_key, v=film)
+
+        question = f"Is {film['title']}, currently your favourite film? (y or n)"
+        answer = input(question)
+        if answer == 'y':
+            db.set(k='favourite', v={**film, **{"date": date_key}})
+
+
+gotta_watch_em_all(weekly_watch_list)
+
--- tests/rgb.py
+++ tests/rgb.py
@@ -1,4 +1,3 @@
-from time import sleep
 from pprint import pprint as pp
 
 from kvdb import Kvdb
@@ -6,26 +5,9 @@ from kvdb import Kvdb
 db = Kvdb()
 db.setup()
 
-db.set('red', {'rgb': [254, 0, 0]})
-sleep(1)
-db.set('blue', {'rgb': [0, 0, 255]})
-sleep(1)
-db.set('green', {'rgb': [0, 255, 0]})
-sleep(1)
-pp(db.get())
-
-sleep(5)
-#  Oops! Not quite red enough.
 db.set('red', {'rgb': [255, 0, 0]})
-print('Updated')
-pp(db.get('red'))
+db.set('blue', {'rgb': [0, 0, 255]})
+db.set('green', {'rgb': [0, 255, 0]})
 
-print('All red:')
-pp(db.get_all('red'))
-
-print('Then red:')
-pp(db.get_first('red'))
-
-print('All things for all keys:')
-pp(db.get(when='all'))
+pp(db.get())
 
